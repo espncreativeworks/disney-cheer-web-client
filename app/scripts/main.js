29c32d6f11c511afbe7907b36467d94c2394e498
Chris Montoro <cmonto1@hotmail.com>
2014-07-22 12:06:49 -0400

    Add audio playback, download

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,31 @@
-/* global jQuery */
+/* global jQuery, document, FastClick, log */
 jQuery(document).ready(function($){
+  'use strict';
+
+  // set logging level
+  log.setLevel(log.levels.TRACE);
+
+  // remove click delay on touch devices
   FastClick.attach(document.body);
+
+  // play audio tracks
+  $('#music').on('click', '[data-control-action="toggle-audio"]', function(e){
+    var $this = $(e.target)
+      , player = $this.siblings('.audio-player')[0];
+
+    if (player.paused){
+      $('#music .audio-player').each(function(){
+        $(this).get(0).pause();
+      });
+      $('#music [data-control-action="toggle-audio"]').each(function(){
+        $(this).text('Listen');
+      });
+      player.play();
+      $this.text('Pause');
+    } else {
+      player.pause();
+      $this.text('Listen');
+    }
+  });
+
 });
