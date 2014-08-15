52c55ba1209b531c61ae907201094f7523c5b4bd
Chris Montoro <cmonto1@hotmail.com>
2014-08-15 19:09:34 -0400

    Resolve undefined event label when downloading PDF

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,20 @@ jQuery(document).ready(function($){
   // remove click delay on touch devices
   FastClick.attach(document.body);
 
+  // initialize Facebook SDK
+  window.fbAsyncInit = function (){
+    FB.init({
+      appId: '671431436258759',
+      status: true,
+      xfbml: true,
+      version: 'v2.0'
+    });
+  };
+
+  // handle sharing
+  $('[data-control-action="share-facebook"]').on('click', onFacebookShare);
+  $('[data-control-action="share-twitter"]').on('click', onTwitterShare);
+
   // play audio tracks
   $('#music').on('click', '[data-control-action="toggle-audio"]', function(e){
     var $this = $(e.target)
@@ -28,16 +42,88 @@ jQuery(document).ready(function($){
     }
   });
 
-  // track audio downloads
-  $('#music').on('click', '[data-control-action="download"]', trackDownload);
+  // track audio downloads, plays
+  $('#music').on('click', '[data-control-action="download"]', trackAudioDownload);
+  $('.audio-player').each(function(){
+    $(this).one('playing', trackPlay);
+  });
 
-  function trackDownload (e){
+  // track routine downloads
+  $('body.guidelines #welcome').on('click', '[data-control-action="download"]', trackGuidelinesDownload);
+
+  function trackAudioDownload (e){
     var $track = $(e.target)
       , type = 'Audio'
       , description = 'Download'
       , label = $track.attr('href');
 
     ga('send', 'event', type, description, label);
+  }
+
+  function trackPlay(e){
+    var $track = $(e.target)
+      , type = 'Audio'
+      , description = 'Play'
+      , label = $track.get(0).currentSrc;
+
+    ga('send', 'event', type, description, label);
+  }
+
+  function trackGuidelinesDownload (e){
+    var $doc = $(this)
+      , type = 'Documents'
+      , description = 'Download'
+      , label = $doc.attr('href');
+
+    ga('send', 'event', type, description, label);
+  }
+
+  function onFacebookShare (e){
+    e.preventDefault();
+    var url = $(this).attr('data-share-url');
+
+    FB.ui({
+      method: 'share',
+      href: url
+    }, function (response){
+      if (response && !response.error_code){
+        ga('send', {
+          hitType: 'social',
+          socialNetwork: 'Facebook',
+          socialAction: 'Share',
+          socialTarget: url,
+          page: document.title
+        });
+      }
+    });
+  }
+
+  function onTwitterShare (e){
+    e.preventDefault();
+
+    var $this = $(this)
+      , params = {}
+      , baseUrl = 'https://twitter.com/share'
+      , tweetWindow = null
+      , tweetWindowUrl = baseUrl + '?'
+      , tweetWindowOptions = 'width=320,height=568,left=' + ($(window).width() - 320) / 2 + ',top=' + ($(window).height() - 568) / 2
+      , url = $this.attr('data-tweet-url');
+
+    params.url = url;
+    params.text = $this.attr('data-tweet-text');
+    params.hashtags = $this.attr('data-hashtags') || '';
+    params.related = $this.attr('data-related-accounts') || '';
+
+    tweetWindowUrl += $.param(params);
+    tweetWindow = window.open(tweetWindowUrl, '_blank', tweetWindowOptions);
+
+    ga('send', {
+      hitType: 'social',
+      socialNetwork: 'Twitter',
+      socialAction: 'Tweet',
+      socialTarget: params.url,
+      page: document.title
+    });
   }
 
 });
